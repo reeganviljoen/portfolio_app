912e961a9458d7c696d480b03794d6e49f0568d4
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-25 17:27:18 +0200

    added new method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,10 @@
 class PortfoliosController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
     @portfolios = Portfolio.all
+  end
+
+  def new 
+    @portfolio = current_user.portfolios.new
   end
 end
