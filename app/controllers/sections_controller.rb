a9c6800e59b311f7df227e249ef357077245a3e0
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-26 12:56:40 +0200

    added new create methods

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,18 @@
 class SectionsController < ApplicationController
+  def new
+    portfolio = Portfolio.find(params[:portfolio_id])
+    @section = portfolio.sections.new
+  end
+
+  def create 
+    # binding.pry 
+    portfolio = Portfolio.find(params[:portfolio_id])
+    @section = portfolio.sections.create!(section_params)
+    redirect_to portfolio
+  end
+
+  private
+  def section_params
+    params.require(:section).permit(:portfolio_id, :title, :body)
+  end
 end
