b5468d08c361d1e1d43892c699a3b7398078e536
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-02 10:03:27 +0200

    added rich text to sections table

--- db/migrate/20220702075941_create_sections.rb
+++ db/migrate/20220702075941_create_sections.rb
@@ -2,7 +2,6 @@ class CreateSections < ActiveRecord::Migration[7.0]
   def change
     create_table :sections do |t|
       t.string :title
-      t.text :body
       t.references :portfolio, null: false, foreign_key: true
       t.timestamps
     end
